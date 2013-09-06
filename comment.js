4f3894a8ec1012188e771d23d5ba84a67143a0ad
lansi951 <eoz754@naver.com>
2013-09-06 22:04:35 +0900

    Update comment.js

--- comment.js
+++ comment.js
@@ -1,14 +1,12 @@
 function loadCommentPage(document_srl, cpage) {
 	// post로 보낼 값들
 	var params = {
-			document_srl: document_srl, 
-			cpage: cpage
+		document_srl: document_srl, 
+		cpage: cpage
 	};
 	var url = current_url.setQuery('mid', current_mid).setQuery('act', 'dispBoardComment');
 	// ajax로 댓글 부분만 새로고침
 	jQuery('.boardComment').load(url+' .feedbackList', params, function(responseText, textStatus, req) {       
-		// 로딩 완료 후 처리
-		initCommentEditor();
-		closeReComment();		
+		// 로딩 완료 후 처리	
 	});
 }
